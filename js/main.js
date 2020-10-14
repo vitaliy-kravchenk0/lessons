9f7f702ac24455e38b19de4b538f12988c469440
Vitaliy <vitaliy.blogger@gmail.com>
2020-10-14 16:48:11 +0300

    Циклы

--- js/main.js
+++ js/main.js
@@ -1,34 +1,23 @@
 "use strict";
 
-if (4 == 9) {
-    console.log("Ok!");
-} else {
-    console.log("Error");
-}
+let num = 50;
 
-// if (num < 49) {
-//     console.log("Error");
-// } else if (num > 100) {
-//     console.log("Много");
-// } else {
-//     console.log("Ok!");
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
 // }
 
-// (num === 50) ? console.log("Ok!") : console.log("Error");
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
 
-const num = "50";
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
 
-switch (num) {
-    case "49":
-        console.log("Неверно");
-        break;
-    case "100":
-        console.log("Неверно");
-        break;
-    case "50":
-        console.log("В точку!");
-        break;
-    default:
-        console.log("Не в этот раз");
-        break;
+    console.log(i);
 }
