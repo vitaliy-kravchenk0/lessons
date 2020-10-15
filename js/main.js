0c77ef4d1fcaa404bd6930ce9f3805bf08dff855
Vitaliy <vitaliy.blogger@gmail.com>
2020-10-15 16:40:42 +0300

    Методы и свойства строк и чисел

--- js/main.js
+++ js/main.js
@@ -1,38 +1,29 @@
 "use strict";
 
-let num = 20;
+const str = "teSt";
+const arr = [1, 2, 4];
 
-function showFirstMessage(text) {
-    console.log(text);
-    console.log(num);
-}
+// console.log(str[2] = "d");
 
-showFirstMessage("Hello World!");
-console.log(num);
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
 
-// console.log(calc(4, 5));
-// console.log(calc(5, 6));
-// console.log(calc(10, 6));
+const fruit = "Some fruit";
 
-// function calc(a, b) {
-//     return (a + b);
-// }
+console.log(fruit.indexOf("q"));
 
-function ret() {
-    let num = 50;
-    return num;
-}
+const logg = "Hello world";
 
-const anotherNum = ret();
-console.log(anotherNum);
+// console.log(logg.slice(6, 11));
 
-const logger = function() {
-    console.log("Hello");
-};
+// console.log(logg.substring(6, 11));
 
-logger();
+console.log(logg.substr(6, 4));
 
-const calc = (a, b) => {
-    console.log("1");
-    return a + b;
-};
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+// console.log(parseInt(test));
+console.log(parseFloat(test));
