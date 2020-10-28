5d60cc8e2ff07375f87e759100a811de536f6b9d
Vitaliy <vitaliy.blogger@gmail.com>
2020-10-28 17:26:56 +0200

    Создаем таймер обратного отсчета на сайте

--- js/main.js
+++ js/main.js
@@ -1,27 +1,94 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const box = document.querySelector(".box");
-    const btn = document.querySelector(".btn");
-    let pos = 0;
 
-    function boxAnimate() {
-        const boxInterval = setInterval(frame, 10);
-        function frame() {
-            if (pos === 300) {
-                clearInterval(boxInterval);
-            } else {
-                pos++;
-                box.style.top = pos + "px";
-                box.style.left = pos + "px";
+    // Tabs
+
+    const tabsParent = document.querySelector(".tabheader__items"),
+        tabs = tabsParent.querySelectorAll(".tabheader__item"),
+        tabsContent = document.querySelectorAll(".tabcontent");
+
+    function hideContent() {
+        tabsContent.forEach(item => {
+            item.classList.add("hide");
+            item.classList.remove("show", "fade");
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove("tabheader__item_active");
+        });
+    }
+
+    function showContent(i = 0) {
+        tabsContent[i].classList.add("show", "fade");
+        tabsContent[i].classList.remove("hide");
+        tabs[i].classList.add("tabheader__item_active");
+    }
+
+    tabsParent.addEventListener("click", (e) => {
+        const target = e.target;
+
+        if (target && target.classList == "tabheader__item") {
+            tabs.forEach((item, i) => {
+                if (item == target) {
+                    hideContent();
+                    showContent(i);
+                }
+            });
+        }
+    });
+
+    hideContent();
+    showContent();
+
+    // Timer
+    
+    const deadline = "2020-10-29";
+
+    function getEndTime(endTime) {
+        const total = new Date(endTime) - new Date(),
+              days = Math.floor(total / (1000 * 60 * 60 * 24)),
+              hours = Math.floor(total / (1000 * 60 * 60) % 24),
+              minutes = Math.floor(total / (1000 * 60) % 60),
+              seconds = Math.floor(total / 1000 % 60);
+
+              return {
+                  total: total,
+                  days: days,
+                  hours: hours,
+                  minutes: minutes,
+                  seconds: seconds
+              };
+    }
+
+    function addZero(num) {
+        if (num >= 0 && num < 10) {
+            return "0" + num;
+        } else {
+            return num;
+        }
+    }
+
+    function getElements(endTime) {
+        const days = document.querySelector("#days"),
+              hours = document.querySelector("#hours"),
+              minutes = document.querySelector("#minutes"),
+              seconds = document.querySelector("#seconds"),
+              timer = setInterval(setTime, 1000);
+
+        setTime();
+
+        function setTime() {
+            const time = getEndTime(endTime);
+
+            days.textContent = addZero(time.days);
+            hours.textContent = addZero(time.hours);
+            minutes.textContent = addZero(time.minutes);
+            seconds.textContent = addZero(time.seconds);
+
+            if (time.total <= 0) {
+                clearInterval(timer);
             }
         }
     }
 
-    btn.addEventListener("click", () => {
-        if (pos === 300 || pos === 0) {
-            pos = 0;
-            boxAnimate();
-        } else {
-
-        }
-    });
+    getElements(deadline);
 });
